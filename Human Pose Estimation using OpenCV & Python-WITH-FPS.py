d30c6537003229b40b240355f760c4616438520d
SANKET CHOUDHARY <sanketchoudhary2002@gmail.com>
2024-06-14 21:47:22 +0530

    Add files via upload

---
+++ Human Pose Estimation using OpenCV & Python-WITH-FPS.py
@@ -0,0 +1,50 @@
+# import necessary packages
+import cv2
+import mediapipe as mp
+import time
+
+# initialize Pose estimator
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+pose = mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5)
+
+# create capture object
+cap = cv2.VideoCapture('4.mp4')
+
+# variables for calculating FPS
+start_time = time.time()
+frame_count = 0
+
+while cap.isOpened():
+    # read frame from capture object
+    _, frame = cap.read()
+
+    try:
+        # convert the frame to RGB format
+        RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # process the RGB frame to get the result
+        results = pose.process(RGB)
+
+        print(results.pose_landmarks)
+        # draw detected skeleton on the frame
+        mp_drawing.draw_landmarks(
+            frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        # calculate and display FPS
+        frame_count += 1
+        elapsed_time = time.time() - start_time
+        fps = frame_count / elapsed_time
+        cv2.putText(frame, f"FPS: {round(fps, 2)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        # show the final output
+        cv2.imshow('Output', frame)
+    except:
+        break
+    if cv2.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
